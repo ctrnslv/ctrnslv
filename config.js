a5863720e291ba258472b7479986c824b2e1c1eb
ctrnslv <87983550+ctrnslv@users.noreply.github.com>
2021-08-15 10:54:03 +0100

    prova

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var driveSlides = 20;
+var driveSlides = 8;
 
 // Number of points on drive route (more = higher quality, but slower to process)
 var driveSmoothness = 100;
@@ -56,11 +56,11 @@ var config = {
         },
       {
             id: 'drive-slide-0',
-            alignment: 'left',
-            hidden: true,
-            title: 'Laveno',
+            alignment: 'centered',
+            hidden: false,
+            title: 'Bari',
             //image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
-          description: 'Cominicia l inchiesta, diretta verso le terre lucane, con lentezza...',
+          description: ' <img src="Cammino.jpg"><br><br>Cominicia l\'inchiesta, diretta verso le terre lucane, con lentezza...',
             location: {
                 // //center: [16.86650, 41.12933],
                 // zoom: 11.29,
@@ -75,30 +75,30 @@ var config = {
         },
 {
       id: 'drive-slide-1',
-      alignment: 'left',
-      hidden: true,
-      title: 'Roma',
+      alignment: 'center',
+      hidden: false,
+      title: 'Bitetto',
       //image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
-      description: 'Nello zaino, lo stretto indispensabile e un ispirazione, il libro di Alliegro Totem Nero',
+      description: 'Nello zaino, lo stretto indispensabile e un\'ispirazione, il libro di Alliegro \"Totem Nero\" che definisce la Basilicata \"una sintesi formidabile di paradossi \[...\], isolata ma centrale, depressa ma ricca, densa di opportunità quanto di angoscia\"<a href="https://www.libreriauniversitaria.it/totem-nero-petrolio-sviluppo-conflitti/libro/9788879755979">Totem Nero</a>',
       location: {
           //center: [16.74949, 41.04042],
           //zoom: 13.34,
           //pitch: 45,
           //bearing: -0.78
-      },
-      //mapAnimation: 'flyTo',
-      rotateAnimation: true,
-      callback: '',
-      onChapterEnter: [],
-      onChapterExit: []
+        },
+        //mapAnimation: 'flyTo',
+        rotateAnimation: true,
+        callback: '',
+        onChapterEnter: [],
+        onChapterExit: []
 },
 {
       id: 'drive-slide-2',
-      alignment: 'left',
-      hidden: true,
-      title: 'Bari',
+      alignment: 'center',
+      hidden: false,
+      title: 'Cassano delle Murge',
       //image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
-      description: 'Nello zaino, lo stretto indispensabile e un ispirazione, il libro di Alliegro Totem Nero',
+      description: '<img src="Quercia secolare.jpg"><br><br>Una guida incontrata lungo la Via Peuceta, da osservatore esterno, mi parla della Basilicata come di \"una terra cuscinetto, di cui non si deve parlare, dove solo i mediocri restano e regna il silenzio.\" Ammiro una quercia secolare la cui pace stride con la confusione che mi aspetta',
       location: {
           // //center: [16.76992, 40.88505],
           // zoom: 16,
@@ -113,11 +113,11 @@ var config = {
     },
     {
           id: 'drive-slide-3',
-          alignment: 'left',
-          hidden: true,
-          title: 'Bitetto',
-          image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
-          description: 'Durante il cammino, ascolto la canzone composta da un cantautore per l inchiesta, al suon di musica avanzo nella murgia afosa',
+          alignment: 'center',
+          hidden: false,
+          title: 'Santeramo in Colle',
+          //image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
+          description: '<iframe width="350" height="250" src="https://www.youtube.com/embed/4sKiwkKBq0U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br>Durante il cammino, ascolto la canzone composta da un cantautore per l\'inchiesta, al suon di musica avanzo nella murgia afosa.',
           location: {
               // center: [16.75535, 40.79241],
               // zoom: 13.34,
@@ -132,11 +132,11 @@ var config = {
         },
         {
               id: 'drive-slide-4',
-              alignment: 'left',
-              hidden: true,
-              title: 'Cassano delle Murge',
+              alignment: 'center',
+              hidden: false,
+              title: 'Altamura',
               //image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
-              description: 'Stridenti contrasti tra un cammino curato e ben segnato, e cumuli di rifiuti che marciscono accanto agli ulivi mi fanno presagire il sapore dell inchiesta',
+              description: '<img src="Rifiuti.jpg"><br><br>Stridenti contrasti tra un cammino curato e ben segnato, e cumuli di rifiuti che marciscono accanto agli ulivi mi fanno presagire il sapore dell\'inchiesta',
               location: {
                   // center: [16.75535, 40.79241],
                   // zoom: 13.34,
@@ -151,11 +151,11 @@ var config = {
             },
             {
                   id: 'drive-slide-5',
-                  alignment: 'true',
-                  hidden: true,
-                  title: 'Santeremo',
+                  alignment: 'center',
+                  hidden: false,
+                  title: 'Gravina di Puglia',
                   //image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
-                  description: 'Incontro Mimmo, sentinella lucana che mi accompagnerà nel percorso dell inchiesta',
+                  description: '<img src="sentinella13-0.jpg"><br><br>Incontro Mimmo, sentinella lucana che mi accompagnerà nel percorso dell\'inchiesta.',
                   location: {
                       // center: [16.42194, 40.81895],
                       // zoom: 13.34,
@@ -170,11 +170,11 @@ var config = {
                 },
                 {
                       id: 'drive-slide-6',
-                      alignment: 'left',
-                      hidden: true,
-                      title: 'Altamura',
-                      image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
-                      description: 'Incontro Nico, insegnante a scuola e sentinella, che mi racconta delle battaglie del Movimento Tutela Val Basento',
+                      alignment: 'center',
+                      hidden: false,
+                      title: 'Pisticci',
+                      //image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
+                      //description: 'Incontro Nico, insegnante a scuola e sentinella, che mi racconta delle battaglie del Movimento Tutela Val Basento',
                       location: {
                           // center: [16.55015, 40.39003],
                           // zoom: 15.17,
@@ -186,44 +186,7 @@ var config = {
                       callback: '',
                       onChapterEnter: [],
                       onChapterExit: []
-                    },
-                    {
-                          id: 'drive-slide-7',
-                          alignment: 'left',
-                          hidden: true,
-                          title: 'Gravina',
-                          image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
-                          description: 'Intervisto l Avvocato Di Pisa, che difende le parti civili nel processo Petrolgate',
-                          location: {
-                              // center: [16.55705, 40.39058],
-                              // zoom: 15.17,
-                              // pitch: 45,
-                              // bearing: 0
-                          },
-                          //mapAnimation: 'flyTo',
-                          rotateAnimation: false,
-                          callback: '',
-                          onChapterEnter: [],
-                          onChapterExit: []
-                        },
-                        {
-                              id: 'drive-slide-7',
-                              alignment: 'left',
-                              hidden: true,
-                              title: 'Pisticci',
-                              image: 'https://lh3.googleusercontent.com/proxy/ftw_SpSYfI1kbOigFYgOtB5khwscLh-aDp_e6lrmNjQiOshyl6vFKrNxuBVsktYgqerp36Sa1LjpVxr43BJ9wqGc-2BxuHC5AiPdpL2IybHJQ9laTx8zocNHbjZBd2iWuN84cqwjAeMquAU4tGd55JX40OKoh17J5H5F',
-                              description: 'Mi presento alla comunità di Pisticci nella loro Chiesa e ascolto le loro voci e denunce',
-                              location: {
-                                  // center: [16.55847,40.39042],
-                                  // zoom: 15.17,
-                                  // pitch: 45,
-                                  // bearing: 0
-                              },
-                              //mapAnimation: 'flyTo',
-                              rotateAnimation: false,
-                              callback: '',
-                              onChapterEnter: [],
-                              onChapterExit: []
+                  
 
                                                                                           }
                                                                                         ]
